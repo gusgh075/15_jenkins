112c56127065223a32c9fb1c0304e67600621499
playdata\playdata2 <gusgh075@gmail.com>
2026-02-20 14:56:42 +0900

    ngrok chk2

--- src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
@@ -18,7 +18,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "argocd 헬스 체체크크";
+        return "argocd 헬스 체체크크 v2";
     }
 
     @GetMapping("/plus")
